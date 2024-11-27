b680f67d4c3d2fcf35a3d94f3fb0e66ac881c1a3
oreninjas <bjbjbjbj2600@gmail.com>
2024-11-27 19:57:52 +0000

    inserted new models

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import dotenv from 'dotenv';
 import connectDB from './db/index.js';
 
 dotenv.config({
-  path: './env',
+    path: './env',
 });
 
-connectDB();
+connectDB()
+    .then(() => {
+        let port = process.env.PORT || 8000;
+        app.listen(port, () => {
+            console.log(`Server is running at port ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error('MongoDB connection failed !! ', err);
+    });
